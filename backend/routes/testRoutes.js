0f219e024be45900dffe4c30c6eb09a70aff9be2
RiverShen31 <tienlam3105@gmail.com>
2024-01-01 22:48:46 +0700

    finish

--- backend/routes/testRoutes.js
+++ backend/routes/testRoutes.js
@@ -7,11 +7,4 @@ const router = express.Router();
 //routes xdd
 router.get("/", testController);
 
-//export
 module.exports = router;
-<<<<<<< HEAD
-
-//test Blood_Donation
-=======
-//LOS RATONES
->>>>>>> origin/Hoang
